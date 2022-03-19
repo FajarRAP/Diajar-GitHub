fe8cd3f2c7927fe017e2557f4af858437089f4b8
FajarRAP <101798306+FajarRAP@users.noreply.github.com>
2022-03-19 20:21:32 +0700

    Bikin Inputan

---
+++ Jarak2Titik.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+  int x1, x2, y1, y2, hasil;
+  cout<<"Masukkan x1 : ";
+  cin>>x1;
+  cout<<"Masukkan y1 : ";
+  cin>>y1;
+  cout<<"Masukkan x2 : ";
+  cin>>x2;
+  cout<<"Masukkan y2 : ";
+  cin>>y2;
+  
+  return 0;
+}
